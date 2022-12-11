da8eb20b35e483fff996d3305e007fc6332acfd6
NoCymer <61873313+NoCymer@users.noreply.github.com>
2022-12-11 04:45:10 +0100

    Comments + Compte Rendu

--- types.h
+++ types.h
@@ -1,12 +1,18 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+/**
+ * Object type containing a name a descitption and an unique index.
+*/
 typedef struct Object{
     char *name;
     char *desc;
     int index;
 }Object;
 
+/**
+ * Location type containing a name a description and unique index a List of pointer to possible exits and a list of contained objects  
+*/
 typedef struct Location {
     char *name;
     char *desc;
@@ -15,6 +21,9 @@ typedef struct Location {
     Object **objects;
 } Location;
 
+/**
+ * Mobile type containing a name a description, its current location and his inventory (List of pointer to objects)
+*/
 typedef struct Mobile
 {
     char *name;
@@ -23,20 +32,22 @@ typedef struct Mobile
     Object **inventory;
 } Mobile;
 
+/**
+ * Stack type containing a location and a pointer to the next element in the stack
+*/
 typedef struct Stack
 {
     Location* location;
     struct Stack* next;
 }Stack;
 
+/**
+ * Game type containing a reference to the player and to the location stack
+*/
 typedef struct Game
 {
     Mobile *player;
     Stack *locationStack;
 } Game;
-
-
-
-
 
 #endif
